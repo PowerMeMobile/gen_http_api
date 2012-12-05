51ee355e75de9a8d2324a9527358785916972b58
Anton Sizov <a.sizov@dev1team.net>
2012-12-05 16:29:13 +0300

    Fix xml

--- src/gen_http_api_converter.erl
+++ src/gen_http_api_converter.erl
@@ -10,6 +10,7 @@
 %% API Functions Definitions
 %% ===================================================================
 
+-spec process(tuple() | [tuple()], binary()) -> {ok, binary()}.
 process(Response, _Suff) when is_binary(Response) ->
 	{ok, Response};
 process(Response, <<"xml">>) ->
@@ -24,13 +25,16 @@ process(Response, _AnySuff) ->
 %% ===================================================================
 
 convert_to_xml(Response) ->
-	?log_debug("Response: ~p", [Response]),
 	Converted = xml_preprocess(Response),
-	?log_debug("Converted: ~p", [Converted]),
-	Xml = xmerl:export_simple(Converted, xmerl_xml,[{prolog, ?xml_prolog}]),
-	?log_debug("Xml: ~p", [Xml]),
+	WithRoot = add_xml_root(Converted),
+	Xml = xmerl:export_simple(WithRoot, xmerl_xml,[{prolog, ?xml_prolog}]),
 	BinXml = unicode:characters_to_binary(Xml),
 	{ok, BinXml}.
+
+add_xml_root(Object = [{_,_} | _Tail]) ->
+	[{object, Object}];
+add_xml_root(Object) ->
+	Object.
 
 xml_preprocess(Atom) when is_atom(Atom) ->
 	[atom_to_list(Atom)];
@@ -38,6 +42,8 @@ xml_preprocess(Integer) when is_integer(Integer) ->
 	[integer_to_list(Integer)];
 xml_preprocess(Float) when is_float(Float) ->
 	[float_to_list(Float)];
+xml_preprocess(Binary) when is_binary(Binary) ->
+	[binary_to_list(Binary)];
 xml_preprocess(List = [{_Key, _Value} | _Rest]) ->
 	[{K, xml_preprocess(V)} || {K, V} <- List];
 xml_preprocess(ObjectList = [ [{_K,_V} | _RestObjectProps] | _RestObjectList]) ->
@@ -48,6 +54,8 @@ xml_preprocess({K, V}) ->
 	[{K, xml_preprocess(V)}];
 xml_preprocess(List = [Item | _Rest]) when is_atom(Item) ->
 	[{item, [atom_to_list(Atom)]} || Atom <- List];
+xml_preprocess(List = [Item | _Rest]) when is_binary(Item) ->
+	[{item, [binary_to_list(Binary)]} || Binary <- List];
 xml_preprocess(Value) ->
 	[Value].
 
@@ -56,14 +64,10 @@ xml_preprocess(Value) ->
 %% ===================================================================
 
 convert_to_json(Object) ->
-	?log_debug("Object: ~p", [Object]),
 	PreProcessedObject = json_preprocess(Object),
-	?log_debug("PreProcessedObject: ~p", [PreProcessedObject]),
 	Json = jsx:term_to_json(PreProcessedObject),
-	?log_debug("Json: ~p", [Json]),
 	{ok, Json}.
 
-%% preprocess object before convert to json
 json_preprocess({K, V}) ->
 	json_preprocess([{K,V}]);
 json_preprocess(null) ->
